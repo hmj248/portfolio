6404c29470573d521b56693d516cb18c06bb0755
Victoria Jeon <victoriahjeon@gmail.com>
2022-01-03 07:25:33 -0500

    fixed navbar and title view for mobile

--- src/App.js
+++ src/App.js
@@ -1,104 +1,118 @@
 import './App.css';
-import { Carousel, Navbar, Container,Offcanvas, Nav, NavDropdown, Col, Row } from 'react-bootstrap';
+import { Carousel, Navbar, Container, Nav, Dropdown} from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Victoria from './Victoria.svg';
 import LinkedIn from './LinkedIn.svg';
 import Title from './Title.svg';
 import GitHub from './GitHub.svg';
-import React, { useState, useEffect, setState } from 'react';
+import TitleSmall from './TitleSmall.svg'
+import React, { useState, useEffect } from 'react';
+import { MdOutlineMenu, MdClose } from 'react-icons/md';
 
-function App(props) {
-  const [width, setWidth] = useState({ width: 0 });
-  const getWindowWidth = () => {
-    return Math.max(
-      document.documentElement.clientWidth,
-      window.innerWidth || 0
-    )
+function App() {
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+  const [open, setOpen] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener('resize', updateWindowWidth)
+  })
+  useEffect(() => {
+    window.addEventListener('resize', updateWindowWidth);
+    return () => {
+      window.removeEventListener('resize', updateWindowWidth)
+    }
+  })
+  const updateWindowWidth = () => {
+    setWindowWidth(window.innerWidth)
   }
-  const onResize = () => {
-    window.requestAnimationFrame(() => {
-      setWidth({
-        width: getWindowWidth()
-      })
-    })
-  }
-  const useComponentWillMount = () => {
-    useEffect(() => {
-      return () => {
-        setWidth({ width: getWindowWidth()})
-      }
-    })
-  }
-  const useComponentDidMount = () => {
-    window.addEventListener('resize', onResize)
-  }
-  const useComponentWillUnmount = () => {
-    window.addEventListener('resize', onResize)
-  }
-  console.log('i am here', props)
+  const hamburgerIcon = <MdOutlineMenu className="Hamburger"
+    onClick={() => setOpen(!open)}
+  /> 
+  const closedIcon = <MdClose className="Hamburger"
+    onClick={() => setOpen(!open)}
+  />
   return (
     <div className="App">
-      <Navbar bg="cream" expand="true" sticky="top">
-        <Container fluid className="App-Header">
-          <Navbar.Brand href="#">
-            <img src={Victoria} alt="logo"/>
-          </Navbar.Brand> 
-          <nav className="Navbar-Links">
-            <Nav.Link className='Navbar-Link' href="http://www.linkedin.com/in/victoria-jeon">
-              <img src={LinkedIn} id="linkedIn-logo" alt="linkedIn-logo"/>
-              <h4 className='link'>LinkedIn</h4>
-            </Nav.Link>
-            <Nav.Link className='Navbar-Link' href="GitHub">
-              <img src={GitHub} id="gitHub-logo" alt="gitHub-logo"/>
-              <h4 className='link'>GitHub</h4>
-            </Nav.Link>
-            {/* <Navbar.Toggle aria-controls="offcanvasNavbar" />
-            <Navbar.Offcanvas
-              id="offcanvasNavbar"
-              aria-labelledby="offcanvasNavbarLabel"
-              placement="end"
-            >
-            <Offcanvas.Header closeButton>
-              <Offcanvas.Title id="offcanvasNavbarLabel">Offcanvas</Offcanvas.Title>
-            </Offcanvas.Header>
-            <Offcanvas.Body>
-              <Nav className="justify-content-end flex-grow-1 pe-3">
-                <Nav.Link href="#action1">Home</Nav.Link>
-                <Nav.Link href="#action2">Link</Nav.Link>
-                <NavDropdown title="Dropdown" id="offcanvasNavbarDropdown">
-                  <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
-                  <NavDropdown.Item href="#action4">Another action</NavDropdown.Item>
-                  <NavDropdown.Divider />
-                  <NavDropdown.Item href="#action5">
-                    Something else here
-                  </NavDropdown.Item>
-                </NavDropdown>
-              </Nav>
-            </Offcanvas.Body>
-            </Navbar.Offcanvas> */}
-          </nav>
-        </Container>
-      </Navbar>
-      {/* <Container className="App-Body">
-      </Container> */}
-      <Container className="Title">
-        <img src={Title} alt="title"/>
-      </Container>
-      <Container className="App-Projects">
-        <Carousel>
-          <Carousel.Item interval={5000}>
-            <img src='/Victoria.png' className='App-logo'alt='App-logo'/>
-          </Carousel.Item>
-          <Carousel.Item interval={4000}>
-          <Nav.Link href="http://good-morels.herokuapp.com/home">
-            <img src='https://giphy.com/embed/yZRoXvpZflasovKcmN' alt='Mushroom'/>
-          </Nav.Link>
-          <Carousel.Caption>
-            {/* <h3>Second Slide label</h3> */}
-          </Carousel.Caption>
-          </Carousel.Item>
-        </Carousel>
-     </Container>
+      {windowWidth < 500 ? 
+      <><Navbar bg="cream" expand="true" sticky="top" className="mobileNav">
+        <nav className="mobileNav"> 
+              {open ? closedIcon : hamburgerIcon}
+              {open && 
+                <ul>
+                <li>
+                  <a href="http://www.linkedin.com/in/victoria-jeon">
+                    <img src={LinkedIn} id="linkedIn-logo" alt="linkedIn-logo" />
+                    <h4 className='link'>LinkedIn</h4>
+                  </a>
+                </li>
+                <li>
+                  <a href="https://github.com/hmj248">
+                    <img src={GitHub} id="gitHub-logo" alt="gitHub-logo" />
+                    <h4 className='link'>GitHub</h4>
+                  </a>
+                </li>
+              </ul> }
+        </nav>
+          <Container fluid className="App-Header">
+            <Navbar.Brand href="#">
+              <img src={Victoria} alt="logo" />
+            </Navbar.Brand>
+            <img src={TitleSmall} alt="title" />
+          </Container>
+        </Navbar>
+          <Container className='AboutMe'>
+            <h1>About Me</h1>
+          </Container>
+          <Container className="App-Projects">
+            <Carousel>
+              <Carousel.Item interval={5000}>
+                <img src='/Victoria.png' className='App-logo' alt='App-logo' />
+              </Carousel.Item>
+              <Carousel.Item interval={4000}>
+                <Nav.Link href="http://good-morels.herokuapp.com/home">
+                  <img src='https://giphy.com/embed/yZRoXvpZflasovKcmN' alt='Mushroom' />
+                </Nav.Link>
+                <Carousel.Caption>
+                  {/* <h3>Second Slide label</h3> */}
+                </Carousel.Caption>
+              </Carousel.Item>
+            </Carousel>
+          </Container></> : 
+ <><Navbar bg="cream" expand="true" sticky="top">
+          <Container fluid className="App-Header">
+            <Navbar.Brand href="#">
+              <img src={Victoria} alt="logo" />
+            </Navbar.Brand>
+            <nav className="Navbar-Links">
+              <Nav.Link className='Navbar-Link' href="http://www.linkedin.com/in/victoria-jeon">
+                <img src={LinkedIn} id="linkedIn-logo" alt="linkedIn-logo" />
+                <h4 className='link'>LinkedIn</h4>
+              </Nav.Link>
+              <Nav.Link className='Navbar-Link' href="https://github.com/hmj248">
+                <img src={GitHub} id="gitHub-logo" alt="gitHub-logo" />
+                <h4 className='link'>GitHub</h4>
+              </Nav.Link>
+            </nav>
+          </Container>
+        </Navbar>
+        <Container className="Title">
+            <img src={Title} alt="title" />
+          </Container>
+          <Container className="App-Projects">
+            <Carousel>
+              <Carousel.Item interval={5000}>
+                <img src='/Victoria.png' className='App-logo' alt='App-logo' />
+              </Carousel.Item>
+              <Carousel.Item interval={4000}>
+                <Nav.Link href="http://good-morels.herokuapp.com/home">
+                  <img src='https://giphy.com/embed/yZRoXvpZflasovKcmN' alt='Mushroom' />
+                </Nav.Link>
+                <Carousel.Caption>
+                  {/* <h3>Second Slide label</h3> */}
+                </Carousel.Caption>
+              </Carousel.Item>
+            </Carousel>
+          </Container></>}
      </div>
     )
   }
