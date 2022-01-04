7a6a94d56947f49a4cb364b1898d0f3ac2a050f1
Victoria Jeon <victoriahjeon@gmail.com>
2022-01-04 11:39:32 -0500

    changed container widths to 100%. Finished sizing and laying out the tech stack

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { Carousel, Navbar, Container, Nav, Dropdown} from 'react-bootstrap';
+import { Carousel, Navbar, Container, Nav, Row, Col } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Victoria from './Victoria.svg';
 import LinkedIn from './LinkedIn.svg';
@@ -7,6 +7,17 @@ import LinkedIn from './LinkedIn.svg';
 import GitHub from './GitHub.svg';
 import TitleSmall from './TitleSmall.svg';
 import Mushroom from './Mushroom.gif';
+import java from './logo-javascript.svg';
+import node from './Node.js_logo.svg';
+import reactIcon from './React.svg';
+import reduxIcon from './redux.svg';
+import htmlCss from './htmlCss.svg';
+import agile from './agileMethodology.svg';
+import sequelize from './sequelize.svg';
+import gcp from './GCP.svg';
+import git from './git.svg';
+import postgres from './postgres.svg';
+import webpack from './webpack.svg';
 import React, { useState, useEffect } from 'react';
 import { MdOutlineMenu, MdClose } from 'react-icons/md';
 
@@ -61,18 +72,18 @@ function App() {
             <img src={TitleSmall} alt="title" />
           </Container>
         </Navbar>
-          <Container className='AboutMe-Mobile'>
+          <Container fluid className='AboutMe-Mobile'>
             <img src='/Victoria.png' className='App-logo-mobile' alt='App-logo' />
             <h1>Hi, I'm Victoria.</h1>
             <p>
-              I'm a Full-stack Engineer with a passion for learning new technologies and exploring innovative solutions. 
+              I'm a Full-stack Web Developer with a passion for learning new technologies and exploring innovative solutions. 
             </p>
           </Container>
-          <Container className="App-Projects-Mobile">
+          <Container fluid className="App-Projects-Mobile">
           <h1>My Projects</h1>
             <Carousel >
               <Carousel.Item interval={4000} >
-                <Container className='project-mobile'>
+                <Container fluid className='project-mobile'>
                   <Nav.Link href="http://good-morels.herokuapp.com/home">
                   <img src={Mushroom} alt='mushroom' className='mushroom-mobile'/>
                   </Nav.Link>
@@ -104,16 +115,16 @@ function App() {
             </nav>
           </Container>
         </Navbar>
-          <Container className='AboutMe'>
+          <Container fluid className='AboutMe'>
             <div>
               <h1>Hi, I'm Victoria.</h1>
               <p>
-              I'm a Full-stack Engineer with a passion for learning new technologies and exploring innovative solutions.
+              I'm a Full-stack Web Developer with a passion for learning new technologies and exploring innovative solutions.
               </p>
             </div>
             <img src='/Victoria.png' className='App-logo' alt='App-logo' />
           </Container>
-          <Container className="App-Projects">
+          <Container fluid className="App-Projects">
           <h1>My Projects</h1>
             <Carousel >
               <Carousel.Item interval={4000} >
@@ -131,7 +142,66 @@ function App() {
                 </Container>
               </Carousel.Item>
             </Carousel>
-          </Container></>}
+          </Container>
+          <Container fluid className='Skills'>
+            <h3>Tech Stack</h3>
+            <Row >
+              <Col md={{ offset: 1}} className="skills">
+                <img src={java} alt='javaScript' className='skills-icon'/>
+                <h5>JavaScript/ES2015</h5>
+              </Col>
+              <Col md={{ offset: 1}} className="skills">
+                <img src={node} alt='expressjs/nodejs' className='skills-icon'/>
+                <h5>ExpressJS & NodeJS</h5>
+              </Col>
+              <Col md={{ offset: 1}} className="skills">
+                <img src={reactIcon} alt='react' className='skills-icon'/>
+                <h5>ReactJS</h5>
+              </Col>
+              <Col md={{ offset: 1}} className="skills">
+                <img src={reduxIcon} alt='redux' className='skills-icon'/>
+                <h5>Redux</h5>
+              </Col>
+            </Row>
+            <Row >
+              <Col md={{ offset: 1}} className="skills">
+                <img src={htmlCss} alt='htmlcss' className='skills-icon'/>
+                <h5>html5/css3</h5>
+              </Col>
+              <Col md={{ offset: 1}} className="skills">
+                <img src={agile} alt='agile' className='skills-icon'/>
+                <h5>Agile Framework</h5>
+              </Col>
+              <Col md={{ offset: 1}} className="skills">
+                <img src={sequelize} alt='sequelize' className='skills-icon'/>
+                <h5>Sequelize</h5>
+              </Col>
+              <Col md={{ offset: 1}} className="skills">
+                <img src={gcp} alt='gcp' className='skills-icon'/>
+                <h5>Google Cloud Platform:</h5>
+                <ul>
+                  <li>Cloud Firestore</li>
+                  <li>Firebase</li>
+                  <li>Compute Engine VM Instances</li>
+                </ul>
+              </Col>
+            </Row>
+            <Row className ="lastRow">
+              <Col md={{ offset: 1}} className="skills">
+                <img src={git} alt='git' className='skills-icon'/>
+                <h5>Git & GitHub Workflow</h5>
+              </Col>
+              <Col md={{ offset: 1}} className="skills">
+                <img src={postgres} alt='postgres' className='skills-icon'/>
+                <h5>PostgreSQL</h5>
+              </Col>
+              <Col md={{ offset: 1}} className="skills">
+                <img src={webpack} alt='webpack' className='skills-icon'/>
+                <h5>Webpack</h5>
+              </Col>
+            </Row>
+            </Container>
+          </>}
      </div>
     )
   }
